90ee10271dd83a4f9dfd8684e7f1382255f1b32c
mangala-karunarathne <mangalaicc@gmail.comm>
2025-05-27 21:25:49 +0530

    Add HomePage component and update package dependencies

---
+++ app/(main)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-red-500 shadow-lg p-8 bg-white rounded-lg">
+      HomePage
+      </h1>
+    </div>
+  )
+}
